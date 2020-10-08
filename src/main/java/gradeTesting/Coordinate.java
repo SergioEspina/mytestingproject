49119e795e580e808c8a613298b63f3d9fefab5a
oriollopezguerra <oriollopezguerra@users.noreply.github.com>
2020-10-08 21:24:46 +0200

    Add Coordinate clases test and normal

---
+++ src/main/java/gradeTesting/Coordinate.java
@@ -0,0 +1,32 @@
+package gradeTesting;
+
+public class Coordinate {
+	private int x;
+	private int y;
+	
+	public Coordinate(){
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Coordinate(int i, int j) {
+		// TODO Auto-generated constructor stub
+		checkCoords(i, j);
+		x = i;
+		y = j;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public boolean checkCoords(int x, int y) {
+		if (x<1 || y<1) {
+			return false;
+		}
+		return true;
+	}
+}
---
+++ src/test/java/gradeTesting/CoordinateTest.java
@@ -0,0 +1,19 @@
+package gradeTesting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CoordinateTest {
+
+	@Test
+	public void testCheckCoords() {
+		Coordinate cord = new Coordinate();
+		boolean res = cord.checkCoords(1, 1);
+		assertEquals(res, true);
+		
+		res = cord.checkCoords(-1, 1);
+		assertEquals(res, true);
+	}
+
+}
