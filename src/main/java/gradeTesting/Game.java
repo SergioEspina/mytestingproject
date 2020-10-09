6215100b9a4e9a797cd40806205ed0cc31d0b6df
MaximilianoVillanuev <MaximilianoVillanuev@DESKTOP-AOGH7Q5>
2020-10-09 09:48:26 +0200

    Game methods

---
+++ src/main/java/gradeTesting/Game.java
@@ -0,0 +1,17 @@
+package gradeTesting;
+
+public class Game {
+
+	
+	private void initGame() {}
+	
+	private boolean isOver() {return false;}
+	
+	public void createPlayer() {}
+		
+	public void play() {}
+	
+	
+	
+	
+}
