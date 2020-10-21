65a202d897e11fc821192349c965868dbea8e496
MaximilianoVillanuev <maximiliano.villanueva@e-campus.uab.cat>
2020-10-21 12:36:35 +0200

    added create players to game class

--- src/main/java/gradeTesting/Game.java
+++ src/main/java/gradeTesting/Game.java
@@ -1,13 +1,73 @@
 package gradeTesting;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Game {
 
+	private ArrayList<Player> players;
+	private int boardRows;
+	private int boardCols;
+	private boolean over;
 	
-	private void initGame() {}
+	public Game(int boardRows, int boardCols) {
+		players = new ArrayList<Player>();
+		this.boardRows = boardRows;
+		this.boardCols = boardCols;
+		over = false;
+	}
 	
-	private boolean isOver() {return false;}
 	
-	public void createPlayer() {}
+	
+	private boolean isOver() {return over;}
+	
+	public void createPlayer() {
+		Board b = new Board(boardRows, boardCols);
+		Player p = new Player(b);
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("number of ships: ");
+		try {
+			/*
+			 * read and create number of ships
+			 */
+			int nShips = Integer.parseInt(in.nextLine());
+						
+			for(int i=0; i<nShips; i++) {
+				
+				ArrayList<Coordinate> shipCoords = new ArrayList<Coordinate>();
+				int x = 0;
+				int y = 0;
+				System.out.println("Ship " + i + " coordinates x, y should be separated by coma and a space between different entries(x,y w,z ....)");
+				String text = in.nextLine();
+				
+				/*
+				 * read and create coordinates for each ship
+				 */
+				for(String coordi : text.split(" ")) {
+					String []xy = coordi.split(",");
+					x = Integer.parseInt(xy[0]);
+					y = Integer.parseInt(xy[1]);
+					shipCoords.add(new Coordinate(x,y));
+				}
+				
+				//add ship to player
+				p.addShip(shipCoords.size(), shipCoords);
+			}
+			
+			//add player to list of players
+			players.add(p);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		System.out.println();
+		
+		
+	
+	}
 		
 	public void play() {}
 	
