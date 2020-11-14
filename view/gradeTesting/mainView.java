5cd238492292c775f14e40ad0a8414691b082147
MaximilianoVillanuev <maximiliano.villanueva@e-campus.uab.cat>
2020-11-14 13:47:40 +0100

    main view

--- view/gradeTesting/mainView.java
+++ view/gradeTesting/mainView.java
@@ -3,49 +3,51 @@ package gradeTesting;
 public class mainView {
 	 
 	private static playerController c;
+	private static CustomOutput o;
 	
-	public mainView(playerController c) {
+	public mainView(playerController c, CustomOutput output) {
 		this.c = c;
+		o = output;
 	}
 	
 	public static void prepareGame() throws Exception {
 		
-		System.out.println("Starting game, size of board is 10x10"); 
-		System.out.println("You have to create 3 ships. The shape of each must be <=4");
-		System.out.println("Player 1 create your ships");
+		o.println("Starting game, size of board is 10x10"); 
+		o.println("each player has 3 ships, the size of each must be <= 4");
+		o.println("Player 1 create and put your ships");
 		c.createPlayer();
 		
-		System.out.println("Player 2 create ships");
+		o.println("Player 2 create and put your ships");
 		c.createPlayer();
 		
 	}
 	
 	public static void printMenu(Player p) {
 		
-		System.out.println("Player " + p.getPlayerNumber() + " is your turn, choose the action:");
-		System.out.println("1. Check my board");
-		System.out.println("2. Check enemy board");
-		System.out.println("3. Atack");
-		System.out.println("4. Surrender");
+		o.println("Player " + p.getPlayerNumber() + " is your turn, choose the action:");
+		o.println("1. Check my board");
+		o.println("2. Check enemy board");
+		o.println("3. Atack");
+		o.println("4. Surrender");
 			
 	}
 	
 	public static void hitBoat(String x, String y) {
-		System.out.println("You hit a ship in position: " + x + "," + y );	
+		o.println("You hit a ship in position: " + x + "," + y );	
 	}
 	
 	public static void playerEliminated(Player p) {
-		System.out.println("Player " + p.getPlayerNumber() + " was eliminated!");
+		o.println("Player " + p.getPlayerNumber() + " was eliminated!");
 	}
 	
 	public static void playerWinner(Player p) {
-		System.out.println("Player " + p.getPlayerNumber() + " wins!");
+		o.println("Player " + p.getPlayerNumber() + " wins!");
 	}
 	
 	public static void printBoard(Board b) {
 		
 		for(int i=0; i<= b.rowSize; i++) {
-			System.out.println();
+			o.println();
 			for(int j=0; j<= b.colSize; j++) {
 				String stringPrint;
 				if(b.getBoard()[i][j] == null) {
@@ -56,10 +58,10 @@ public class mainView {
 				System.out.print("|" + stringPrint + "|");
 			}
 			
-			System.out.println();
+			o.println();
 			System.out.print("");
 			for(int j=0; j<=b.colSize; j++) System.out.print(" _ ");
-			System.out.println();
+			o.println();
 		}
 	}
 }
